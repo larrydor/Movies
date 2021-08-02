76936eed445927addf47c00684d8514da14d379e
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-02 17:51:31 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -5,6 +5,13 @@ import json
 import http.client
 from dotenv.main import load_dotenv
 load_dotenv()
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", "OOPS, please create an environment variable called 'SENDGRID_API_KEY'")
+MY_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS", "OOPS, please create an environment variable called 'SENDER_EMAIL_ADDRESS'")
+SENDGRID_TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID", default="OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
 API_KEY = os.getenv("RAPID_API_KEY")
 
 print("-------------------------")
@@ -65,3 +72,56 @@ def listToString(s):
 
 print(listToString(film_name))
 print(film_name, film_year)
+
+
+
+
+
+
+
+
+print("Would you like an emailed copy of this research?")
+user_email_address = input("Please input your email address, or 'N' to opt-out: ")
+
+EMAIL_ADDRESS = user_email_address
+
+if user_email_address.upper() in ["N", "NO", "N/A"]:
+    print("You've elected to not receive an emailed copy.")
+    print("Thank You!")
+elif "@" not in user_email_address:
+    print("You have entered an invalid email address.")
+else:
+    print(f"Sending copy to {EMAIL_ADDRESS}.")
+
+    email_products = []
+    for items in range(len(selected_ids)):
+        if not selected_ids[items].isnumeric():
+            email_product = selected_ids[items]
+            email_product["name"] = items["name"]
+            email_product["price"] = to_usd(items["price"])
+            email_products.append(email_product)
+        #source: https://stackoverflow.com/questions/10631473/str-object-does-not-support-item-assignment-in-python
+
+    receipt = {
+        "subtotal_price_usd": to_usd(total_purchase),
+        "tax_price_usd": to_usd(tax_total),
+        "total_price_usd": to_usd(total_total),
+        "human_friendly_timestamp": now,
+        "products": email_products
+    }
+    
+    client = SendGridAPIClient(SENDGRID_API_KEY)
+
+    message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=user_email_address)
+    message.template_id = SENDGRID_TEMPLATE_ID
+    message.dynamic_template_data = receipt
+    response = client.send(message)
+
+    if str(response.status_code) == "202":
+        print("Email sent successfully!")
+    else:
+        print("Oh, something went wrong with sending the email.")
+        print(response.status_code)
+        print(response.body)
+
+    print("Thank You!")
